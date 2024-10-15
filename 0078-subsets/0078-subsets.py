a42a2f6c96c637274692ea9e70a7877b831f0dfc
sandeepsalwan1 <118837112+sandeepsalwan1@users.noreply.github.com>
2024-10-15 12:09:35 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,27 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # res = []
-        # [1,2,3]
-        subset = []
-        res = []
-        def dfs(i):
-            if i == len(nums):
+        subset,res = [],[]
+
+    #12 
+    #. 
+        def backtracking(i):
+            if i == len(nums): 
                 res.append(subset.copy())
-                return 
-            subset.append(nums[i])
-            dfs(i+1)
+                return
+            subset.add(nums[i])
+            backtracking(i+1)
 
             subset.pop()
-            dfs(i+1)
-        dfs(0)
-        return res
+            backtracking(i+1)
 
-        # def backtracking(input):
-        #     if input 
-        #     for i in input:
-        #         if i == 0:
-        #             return
-        # def testBack():
 
-                   
-        # return backtracking(nums)
+        backtracking(0)
