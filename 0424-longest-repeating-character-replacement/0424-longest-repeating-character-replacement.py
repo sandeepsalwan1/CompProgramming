e67e9df6c42d94a4ae8c4413a5088d6bca9d187a
sandeepsalwan1 <118837112+sandeepsalwan1@users.noreply.github.com>
2024-11-17 08:37:35 -0500

    Time: 91 ms (22.54%), Space: 16.8 MB (24.73%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,39 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        if not s: return 0
-        res = 1
-        for c in range(len(s)):
-            newDict = {}
-            newDict[s[c]] = newDict.get(s[c],0) + 1
-            for r in range(c+1, len(s)):
-                newDict[s[r]] = newDict.get(s[r],0) + 1
-                input = list(newDict.values())
-                print(input)
-                input.sort(reverse = True)
-                if(len(input) > 1 and (sum(input)-input[0]) > k): 
-                    break
-                inputVal = sum(input)
-                res = max(res, inputVal)
+        res = 0
+        l,r=0,0
+        currDict = {}
+        while r < len(s):
+            currDict[s[r]] = currDict.get(s[r],0 ) + 1
+            while ((r-l +1 )- max(currDict.values() ) > k) and l < len(s):
+                currDict[s[l]] = currDict.get(s[l],0 ) - 1
+                l+=1
+            res = max(res, r-l+1)
+            r+=1
         return res
+
+
+                # for c in range(len(s)):
+        #     newDict = {}
+        #     newDict[s[c]] = newDict.get(s[c],0) + 1
+        #     for r in range(c+1, len(s)):
+        #         newDict[s[r]] = newDict.get(s[r],0) + 1
+        #         input = list(newDict.values())
+        #         input.sort(reverse = True)
+        #         if(len(input) > 1 and (sum(input)-input[0]) > k): 
+        #             break
+        #         inputVal = sum(input)
+        #         res = max(res, inputVal)
+
+
+                # for c in range(len(s)):
+        #     newDict = {}
+        #     newDict[s[c]] = newDict.get(s[c],0) + 1
+        #     for r in range(c+1, len(s)):
+        #         newDict[s[r]] = newDict.get(s[r],0) + 1
+        #         input = list(newDict.values())
+        #         input.sort(reverse = True)
+        #         if(len(input) > 1 and (sum(input)-input[0]) > k): 
+        #             break
+        #         inputVal = sum(input)
+        #         res = max(res, inputVal)
