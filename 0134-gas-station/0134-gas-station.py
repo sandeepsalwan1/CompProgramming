dc00294fc277087df2a35f62a875e1e7226ceb43
Sandeep Salwan <salwansandeep5@gmail.com>
2025-01-13 21:21:03 -0500

    Time: 15 ms (90.07%), Space: 23.4 MB (27.4%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -2,16 +2,33 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         #subtract current gas with cost 
         #iterate to the split which is current. tank + gas 
-
-        total_gas = 0
-        start, current_gas = 0,0
+        if sum(gas) < sum(cost):
+            return -1
+        
+        start, tank = 0,0
+        
         for i in range(len(gas)):
-            fuel_gain = gas[i] - cost[i]
-            total_gas += fuel_gain
-            current_gas += fuel_gain
-
-            if current_gas < 0:
-                current_gas = 0
+            tank += gas[i] - cost[i]
+            if tank < 0:
                 start = i + 1
+                tank = 0
+        
+        return start
+        # diff = []
+        # for i in range(len(gas)):
+        #     diff.append(gas[i] - cost[i])
+        # currVal = 0
+        # for i in range(len(gas)):
+        #     curVal += gas[i] 
+        # total_gas = 0
+        # start, current_gas = 0,0
+        # for i in range(len(gas)):
+        #     fuel_gain = gas[i] - cost[i]
+        #     total_gas += fuel_gain
+        #     current_gas += fuel_gain
 
-        return -1 if total_gas < 0 else start
+        #     if current_gas < 0:
+        #         current_gas = 0
+        #         start = i + 1
+
+        # return -1 if total_gas < 0 else start
