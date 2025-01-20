bdd7dff733211f7cd200f36d402ad5a00c494561
Sandeep Salwan <salwansandeep5@gmail.com>
2025-01-19 20:13:19 -0500

    Time: 5 ms (61.29%), Space: 20.8 MB (30.91%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -4,74 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
+root
+
+
+depth 1 = 0 
+
+everytime we iterate we add 1 to depth 
+maxDiam = 0
+dfs params: curr, diam
+
+everytime we iterate to left/right we'll add 1 to diam
+every iteration we'll compare maxDiam with curr depth
+
+'''
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        """
-        root.left,root.right
-        root.left,root.left
-        """
-        # res = 0 
-        # def calcDepth(root):
-        #     if not root: return 0
-        #     return 1 + max(calcDepth(root.left), calcDepth(root.right))
-
-        '''
-        
-        '''
-        self.res = 0
+        self.maxDiam = 0
+        # height = max(l,r) + 1
         def dfs(curr):
-            if not curr: 
-                return 0
-
+            if not curr: return -1
             left = dfs(curr.left)
-            right = dfs(curr.right)
-            self.res = max (self.res, left + right)
-            return 1 + max(left,right)
+            right = dfs(curr.right) 
+            diam = left + right+ 2 
+            self.maxDiam = max(self.maxDiam, diam)
+            return 1+ max(left, right)
         dfs(root)
-        return self.res
-
-        # def dfs(r):
-        #     dfs(r.left)
-        #     dfs(r.right)
-        #     return 1 + 
+        return self.maxDiam
 
 
-        # #set vars
-        # nodeList, nodeDict = [], defaultdict(list)
-        # #dfs
-        # def dfs(node, nodeList, nodeDict):
-        #     if not node:
-        #         return
-        #     nodeList.append(node.val)
-        #     if not node.left and not node.right:
-        #         nodeDict[id(node)].append(nodeList.copy())
-        #     else:
-        #         if node.left:
-        #             dfs(node.left, nodeList, nodeDict)
-        #         if node.right:
-        #             dfs(node.right, nodeList, nodeDict)
-        #     nodeList.pop()
-        # #calc depth
-        # def calcDepth(root):
-        #     if not root: return 0
-        #     return 1 + max(calcDepth(root.left), calcDepth(root.right))
-        # maxLength = calcDepth(root) - 1
-        # #call
-        # dfs(root,nodeList,nodeDict)
-        # #iter through values, see if max larger than current length from point a to b
-        # output = list(nodeDict.values())
-        # print(nodeDict.values())
-        # print(output)
-        # for i in range(len(output)):
-        #     for j in range(len(output[i])):
-        #         first, innerCount = 0, 0 
-        #         for k in range(i+1, len(output)):
-        #             if j >= len(output[k]): break
-        #             if output[i][j] != output[k][j]: 
-        #                 if(first == 0): 
-        #                     first += 1
-        #                     innerCount +=2
-        #                 else: 
-        #                     innerCount +=1
-        #         maxLength = max(maxLength, innerCount)
-        # return maxLength
+        dfs(root,0)
+        return self.maxDiam
