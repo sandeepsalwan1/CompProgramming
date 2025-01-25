c54da26f0163b5733ceb7e258ee2f629d9603518
Sandeep Salwan <salwansandeep5@gmail.com>
2025-01-25 14:07:00 -0500

    Time: 15 ms (8.65%), Space: 18.1 MB (16.64%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -21,50 +21,47 @@ edge cases: if invalid VVVV make sure that my sioliuion can account for numerals
 '''
 class Solution:
     def intToRoman(self, num: int) -> str:
-        hash = {}
         # for i in num: 
-        hash.update( {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"} ) 
         res = ""
+        hash = {}
+        hash.update( {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"} ) 
         num = str(num)
+        # 2500
+        # M
+        # 1000
+        
         for i in range(len(num)):
             zeroList = [0] * (len(num) - i-1)
             CurrVal = num[i]
-            for i in zeroList:
-                CurrVal += str(i)
+            for z in zeroList:
+                CurrVal += str(z)
             CurrVal = int(CurrVal)
             if CurrVal == 0: continue
-            maxNum = "I"
-            
-            maxVal = 0
-            for k in hash.keys():
-                if (k) <= CurrVal:
-                    maxNum = hash[k]
-                    maxVal = k
-                else: break
-            print(maxNum, maxVal)
-
-
-            target = CurrVal
-            if CurrVal in [4,9,40,90,400,900]:
-                if CurrVal == 4: res+=("IV")
-                elif CurrVal == 40: res+=("XL")
-                elif CurrVal == 400: res+=("CD")
-                elif CurrVal == 9: res+=("IX")
-                elif CurrVal == 90: res+=("XC")
-                elif CurrVal == 900: res+=("CM")
-            elif maxNum in ["V","L","D"]:
-                # carryOver = maxVal
-                res += (maxNum)
-                # carryOver -= maxVal 
-                continue
-            else:
-                target = CurrVal
-                while target >= maxVal and maxVal!= 0:
+            while CurrVal > 0:
+            # target = CurrVal
+                if CurrVal in [4,9,40,90,400,900]:
+                    if CurrVal == 4: res+=("IV")
+                    elif CurrVal == 40: res+=("XL")
+                    elif CurrVal == 400: res+=("CD")
+                    elif CurrVal == 9: res+=("IX")
+                    elif CurrVal == 90: res+=("XC")
+                    elif CurrVal == 900: res+=("CM")
+                    CurrVal = 0
+                else:
+                    target = CurrVal
+                    maxNum = "I"
+                    maxVal = 0
+                    for k in hash.keys():
+                        if (k) <= CurrVal:
+                            maxNum = hash[k]
+                            maxVal = k
+                        # else: break
+                        # while target >= maxVal and maxVal!= 0:
                     res+= (maxNum)
-                    target -= maxVal
+                    CurrVal -= maxVal
 
-        #         check next and check prev 
- 
+            #         check next and check prev 
+    
 
 
         return res
