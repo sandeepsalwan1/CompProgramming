ec230702c5f9d122e25907cdd25b879fad120ce8
Sandeep Salwan <salwansandeep5@gmail.com>
2025-01-28 19:09:44 -0500

    Time: 0 ms (100%), Space: 18 MB (40.53%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,32 +1,3 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # perms = [[]]
-        # res = []
-        # for n in nums:
-        #     subset = []
-        #     for p in perms:
-        #         for i in range(len(perms) + 1):
-        #             subsetCopy = p.copy()
-        #             subsetCopy.insert(i,n)
-        #             res.append(subset.copy)
-        #     perms.append(subset)
-
-        # # [] 3  32 23   132  312 321.      123 213 231
-        # return res
-       
-        if len(nums) == 0: return [[]]
-        perms = self.permute(nums[1:])
-        res = []
-        # for n in nums:
-        #     subset = []
-        for p in perms:
-            for i in range(len(p) + 1):
-                subsetCopy = p.copy()
-                subsetCopy.insert(i,nums[0])
-                res.append(subsetCopy)
-        return res
-
-
-        # [] 3  32 23   132  312 321.      123 213 231
-        # return res
-                    
+        import itertools;return list(itertools.permutations(nums))
