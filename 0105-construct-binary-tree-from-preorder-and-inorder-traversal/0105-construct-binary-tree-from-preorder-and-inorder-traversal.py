80e83eff2e70f379addbc11c6efc00415d9c1d42
Sandeep Salwan <salwansandeep5@gmail.com>
2025-02-05 19:42:51 -0500

    Time: 94 ms (47.06%), Space: 89.3 MB (19.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,31 +6,25 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder: return None
-        root = TreeNode(preorder[0])
+        if not preorder or not inorder: return
         mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        root = TreeNode(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1],inorder[0:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
+
+
         return root
 
-        
-        # dfs left and right to  
-        # # 3 is root. put the root as node. then observe that inorder[-1] is rightmost node and inorder[-2] os parent 
-        # res = []
-        # root = TreeNode(preorder[0])
-        # mid = inorder.index(preorder[0])
-        # root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        # root.right =self.buildTree(preorder[mid+1:],inorder[mid+1:] )
-        # return root
-        # # def dfs(curr, left, right):
-        # #     print(curr)
-        # #     # m = 
 
-        # return res
 
-            
-            #    1
-            #  2.  4
-            # 3 7   5 6
 
-            # pre: 1237456. in: 3271546
+#         3
+
+#     9      20
+
+#   1.    2   15    7
+# 8
+
+# 3 9  1 8 2 20 15 7 
+
+# 8 1 9 2 3 15 20 7
