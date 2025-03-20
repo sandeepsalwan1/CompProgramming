bc850bea2cea7f658c367107d676015fa2f3e934
Sandeep Salwan <salwansandeep5@gmail.com>
2025-03-19 22:26:02 -0400

    Time: 0 ms (100%), Space: 19.8 MB (31.04%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,20 +1,70 @@
 # Definition for a binary tree node.
+        
+# if not root: return True
+# return  dfs(root) and self.isValidBST(root.left) and self.isValidBST(root.right) 
+
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+        # dfs  O(n) 
+        # space O(log n)
+        # node left less than curr
+        # node right greater than curr
+
+        
+    #         5 
+    #      1       4
+    #           3     6
+    #  l, 5      5, r (inf)
+
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def dfs(curr,minVal,maxVal):
+        def dfs(curr, l, r):
             if not curr: return True
-            if curr.val <= minVal or curr.val >= maxVal : return False
-            return dfs(curr.left, minVal, curr.val) and dfs(curr.right, curr.val, maxVal)
+            if curr.val <= l or curr.val >= r: return False
+            return dfs(curr.left, l, curr.val) and dfs(curr.right, curr.val, r)
+        return dfs(root, float('-inf'), float('inf'))
 
-            # if curr.right and curr.right.val <= curr.val: return False
-            # return dfs(curr.left,root) or dfs(curr.right,root)
-        return dfs(root, float('-inf'), float('inf')) 
-        
-        # is False: return False
-        # # return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def dfs(curr,l,r):
+        #     if not curr: return True
+        #     if curr.val <= l or curr.val >= r: return False
+        #     return dfs(curr.left, l, curr.val) and dfs(curr.right, curr.val, r)
+        # return dfs(root, float('-inf'), float('inf'))
+
+
+
+
+
+
+
+
+        # def dfs(curr, l, r):
+        #     if not curr: return True 
+        #     if curr.val <= l or curr.val >= r : return False
+        #     return dfs(curr.left,l, curr.val) and dfs(curr.right, curr.val, r)
+
+        # return dfs(root, float('-inf'), float('inf'))
+
