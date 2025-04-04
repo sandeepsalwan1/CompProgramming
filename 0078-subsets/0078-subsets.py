4bb069b0341c47683ec608535e6f123db9d17691
Sandeep Salwan <salwansandeep5@gmail.com>
2025-04-04 14:44:00 -0400

    Time: 0 ms (100%), Space: 17.9 MB (40.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,58 @@
+# insert in any order so I will 
+
+# start 
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        subset = []
-        def dfs(n):
-            if n >= len(nums):
-                res.append(subset.copy())
-                return
-            subset.append(nums[n])
-            dfs(n+1)
-            subset.pop()
-            dfs(n+1)
-        
-        dfs(0)
+        output = [list(itertools.combinations(nums,i)) for i in range(len(nums) + 1)]
+        for i in output:
+            for j in i:
+                res.append(j)
         return res
+#         self.output = []
+#         def backtracking(i,cur):
+#             if not cur: return [[]]
+            
+
+#         backtracking[0, nums]
+#         return self.output
+
+# base case: i >= len(nums)
+
+
+# backtrack approach:
+# self.output = []
+# backtracking(i+1, cur)
+# cur.append(nums[i])
+# if not cur: return [[]]
+# cur.popleft()
+# for loop
+# backtracking(i+1, cur)
+
+# cur.append()
+# [3]
+
+
+
+
+# f1 
+# 123
+
+
+# f2 
+# 23
+
+# f3 
+# 2, 3
+
+
+
+
+# 3
+
+
+# 13
+
+
+# 32
