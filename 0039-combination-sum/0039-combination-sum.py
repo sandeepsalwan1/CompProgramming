ad5b921f60f57031ec5e62fe7c0fe1e816c4f848
Sandeep Salwan <salwansandeep5@gmail.com>
2025-04-08 17:17:40 -0400

    Time: 11 ms (58.95%), Space: 18 MB (20.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,81 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res, subset = [],[]
-        def backtracking(i, sumVal):
-            if i >= len(candidates) or sumVal > target: 
+        res, cur = [],[]
+        def backtracking(i,curVal):
+            if i >= len(candidates) or curVal> target: return
+            if curVal == target: 
+                res.append(cur.copy())
                 return
-            if sumVal == target:
-                res.append(subset.copy()) # [:]
-                return
-            subset.append(candidates[i])
-            backtracking(i, sumVal + candidates[i])
-            subset.pop()
-            backtracking(i + 1, sumVal)
+            cur.append(candidates[i])
+            backtracking(i, curVal + candidates[i])
+            cur.pop()
+            backtracking(i+1, curVal)
+        
         backtracking(0,0)
         return res
 
-        # 2367
-        # 2 2 2 3
-        # res = 
-        # i = 
+#                 2   
+#             2     27mk,                                                                                           
+#         2 6
+#     2    2223
+# 2       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         res =[]
+#         numberTimes = target//min(candidates) + 1
+#         while numberTimes > 0:  
+#             for p in itertools.product(candidates, repeat = numberTimes):
+#                 if sum(p) == target: 
+#                     val = list(p)
+#                     val.sort()
+#                     if val not in res:
+#                         res.append(val)
+
+#             numberTimes -=1
+#         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         # stack = []
+#         # res = []
+
+#         # def combination(idx, val):
+#         #     if idx >= len(candidates) or val >= target: return
+#         #     if val == target: 
+#         #         res.append(stack.copy()) 
+#         #         return
+            
+#         #     stack.append(candidates[idx])
+#         #     combination(idx, val+ candidates[idx])
+#         #     stack.pop()
+#         #     combination(idx+1, val - candidates[idx])
+
+#         # combination(0,0)
+#         # return res
