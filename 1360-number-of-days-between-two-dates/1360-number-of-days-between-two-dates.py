9c5565f6e1aeadf68bbd50b56f24f16591c0f6e1
Sandeep Salwan <salwansandeep5@gmail.com>
2025-08-16 22:51:51 -0700

    Time: 0 ms (100%), Space: 17.8 MB (84.83%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
@@ -1,7 +1,32 @@
-import datetime
-
 class Solution:
     def daysBetweenDates(self, date1: str, date2: str) -> int:
-        y1, m1, d1 = map(int, date1.split('-'))
-        y2, m2, d2 = map(int, date2.split('-'))
-        return abs((datetime.date(y1, m1, d1) - datetime.date(y2, m2, d2)).days)
+        months = [31,28,31,30,31,30,31, 31,30,31,30,31]
+        def findDate(input):
+            curNum = 0
+            curList = []
+            for i in input:
+                if i.isdigit():
+                    curNum = curNum * 10 + int(i)
+                else:
+                    curList.append(curNum)
+                    curNum = 0
+            curList.append(curNum)
+            y,m,d = curList[0], curList[1], curList[2]
+
+            cur, toDays, i = m, 0, 0
+            while cur != 1:
+                cur -= 1
+                toDays += months[i]
+                i += 1
+
+            toDays += d
+            toDays += 365 * y
+
+            # minimal leap fix (two lines + 1 if):
+            toDays += ( (y-1)//4 - (y-1)//100 + (y-1)//400 )   # leap days from *previous* full years
+            if m > 2 and (y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)):  # after Feb in a leap year
+                toDays += 1
+
+            return toDays
+
+        return abs(abs(findDate(date1)) - abs(findDate(date2)))
