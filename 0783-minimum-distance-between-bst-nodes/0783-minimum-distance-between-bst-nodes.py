6f1ca57c320b66fb1e89f34f0f7bf3f5882b6cf3
Sandeep Salwan <salwansandeep5@gmail.com>
2025-08-28 23:12:57 -0700

    Time: 0 ms (100%), Space: 17.6 MB (92.76%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -1,36 +1,58 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        self.first, self.second  = -10000, float('inf')
-        curLis = []
-        def dfs(cur):
-            if not cur: return
-            dfs(cur.left)
-            curLis.append(cur.val)
-            dfs(cur.right)
+        ans = float('inf')
+        self.prev = None
+        def dfs(node):
+            nonlocal ans
+            if not node:
+                return
+            # if self.prev
+            dfs(node.left)
+
+            if self.prev:ans = min(ans, node.val - self.prev.val)
+            self.prev = node
+
+            # if preve:
+            dfs(node.right)
+
+
         dfs(root)
-        prevRes = float('inf')
-        for i in range(len(curLis)-1):
-            prevRes = min(abs(curLis[i+1]-curLis[i]), prevRes)
-        return prevRes
-        # print(second-first)
-        def bfs(cur):
-            q = deque([cur])
-            # seen = se
-            while q:
-                for i in range(len(q)):
-                    child = q.popleft()
-                    if child.left: q.append(child.left)
-                    if child.right: q.append(child.right)
-                    print(self.first,self.second)
-                    if abs(child.val - self.first) < abs(self.second - self.first):
-                        self.first,self.second = child.val,self.first
-                    print(self.first,self.second)
+        return ans
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+#         self.first, self.second  = -10000, float('inf')
+#         # curLis = []
+#         self.prevRes = float('inf')
+#         def dfs(cur,prev):
+#             if not cur: return 0
+#             dfs(cur.left,cur.val)
+#             self.prevRes = min(abs(prev-cur.val), self.prevRes)
+            
+#             dfs(cur.right,cur.val)
+#         dfs(root, float('inf'))
+#         # for i in range(len(curLis)-1):
+#         return self.prevRes
+#         # print(second-first)
+#         def bfs(cur):
+#             q = deque([cur])
+#             # seen = se
+#             while q:
+#                 for i in range(len(q)):
+#                     child = q.popleft()
+#                     if child.left: q.append(child.left)
+#                     if child.right: q.append(child.right)
+#                     print(self.first,self.second)
+#                     if abs(child.val - self.first) < abs(self.second - self.first):
+#                         self.first,self.second = child.val,self.first
+#                     print(self.first,self.second)
                 
-            return abs(self.second - self.first)
-        return bfs(root)
+#             return abs(self.second - self.first)
+#         return bfs(root)
