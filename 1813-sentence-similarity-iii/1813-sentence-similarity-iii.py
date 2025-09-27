c0485906b62018fcb41d25f66d6312d97f9a34f1
Sandeep Salwan <salwansandeep5@gmail.com>
2025-09-27 14:40:03 -0400

    Time: 0 ms (100%), Space: 17.9 MB (19.07%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -6,9 +6,28 @@ class Solution:
         w1 = sentence1.split()
         w2 = sentence2.split()
 
-        # If same length but not identical, can't fix by adding words to ends
-        if len(w1) == len(w2):
-            return False
+        if len(w1)> len(w2):
+            w1,w2=w2,w1
+        l1 = 0
+        while l1 < len(w1) and w1[l1]==w2[l1]:
+            l1+=1
+
+        l2,r2 =len(w1)-1, len(w2)-1
+        while l2 >= 0 and r2>= 0 and w1[l2]==w2[r2]:
+            l2-=1
+            r2-=1
+
+        return l1 > l2
+
+
+
+
+
+
+
+
+
+
 
         # Make sure w_short is the shorter list of words
         w_short, w_long = (w1, w2) if len(w1) < len(w2) else (w2, w1)
