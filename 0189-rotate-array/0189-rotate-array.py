07b9fe83e55032cce4d1a84a473d792de4edbcac
Sandeep Salwan <salwansandeep5@gmail.com>
2025-09-27 21:30:42 -0400

    Time: 4 ms (58.62%), Space: 25.5 MB (91.41%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -12,17 +12,42 @@
 # nums[l] 
 
 
-
+print(0%5)
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
-                # addThis = nums[:k+1]
-        # nums.extend(addThis)
-        # nums = (nums[k:])
+        k%= len(nums)
+        nums[:] = nums[-k:] + nums[:-k] 
+
+
+
+
+
+
+
+        # k%= len(nums)
+        # nums[:] = nums[-k:] + nums[:-k]
+        # if k==0: return nums
+        # print(nums[len(nums)-k:len(nums)], nums[:len(nums)-k])
+        # print(nums)
+        # nums = (nums[len(nums)-k:len(nums)] + nums[:len(nums)-k])
+        # print(nums)
+
+        # return nums
+        # # return nums[len(nums)-k:len(nums)] + nums[:len(nums)-k]
+
+
+
+
+
+
+        # # k %= len(nums)
+        # nums[:] = nums[-k:] + nums[:-k]
+        #         # addThis = nums[:k+1]
+        # # nums.extend(addThis)
+        # # nums = (nums[k:])
 
         
