d2feea4b4bcc586bbd1d084f02d124cff8b3d504
Sandeep Salwan <salwansandeep5@gmail.com>
2025-10-09 00:07:33 -0400

    Time: 83 ms (59.8%), Space: 31.2 MB (53.02%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
@@ -1,39 +1,26 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         nums.sort()
-        n = len(nums)
-        
-        # Calculate prefix sums for efficient sum calculation
-        prefix_sum = [0] * (n + 1)
-        for i in range(n):
-            prefix_sum[i + 1] = prefix_sum[i] + nums[i]
-        
-        max_perimeter = -1
-        
-        # Try to form polygons starting from smallest possible (3 sides)
-        # Check each position as potential longest side
-        for i in range(2, n):  # i is the index of the longest side
-            # Sum of all sides except the longest (nums[i])
-            sum_of_others = prefix_sum[i]
-            
-            # Check polygon inequality: sum of other sides > longest side
-            if sum_of_others > nums[i]:
-                # Valid polygon found, calculate perimeter
-                perimeter = sum_of_others + nums[i]
-                max_perimeter = perimeter  # Keep updating as we go (larger perimeters come later)
-        
-        return max_perimeter
-    #     nums.sort()
-    #     cur = 0
-    #     cnt =0
-    #     if len(nums) == 3 and nums[-1] >= nums[0]+nums[1]: return -1
-    #     for i in nums:
-    #         if cnt >=3: 
-    #             if i >= cur: break
-    #         cur+=i
-    #         cnt+=1
+        cur = 0
+        cnt =0
+        res = -1
+        total = 0
+        # if len(nums) == 3 and nums[-1] >= nums[0]+nums[1]: return -1
+        for i in nums:
+            if total > i: res = total +i
+            total +=i
+        return res
 
-    #     return cur 
+        #     if cnt >=3: 
+        #         if i >= cur: break
+        #     cur+=i
+        #     cnt+=1
 
-    # Sort the array in ascending order
-  
+        # return cur 
+
+
+# 1 1 2
+
+
+# 1 1 
+# total > num
