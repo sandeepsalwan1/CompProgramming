76a64c19f3de7113f52bb2467ebbb8475c4ab5d3
Sandeep Salwan <salwansandeep5@gmail.com>
2025-10-14 22:19:29 -0400

    Time: 0 ms (100%), Space: 19.9 MB (35.22%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,82 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) <= 1: return True
+        intervals.sort(key = lambda x: x[0])
+        
+        newInterval= intervals[0]
+        for i,(a,b) in enumerate(intervals):
+            if i ==0:
+                continue
+            
+            if newInterval[1]> a and newInterval[0] <= b:
+                # newInterval[1] = max(newInterval[1],b)
+                return False
+            else:
+                newInterval = [a,b]
+        return True
+
+
+            # [1,10] [3,11]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # intervals.sort(key = lambda x: x[0])
+        # lastEnd = float('-inf')
+        # for start,end in intervals:
+        #     if start < lastEnd: return False
+        #     lastEnd= end
+        # return True
+
+
+# -------
+
+#  --- 
+#      --
+ 
+# [[0,2],[5,10],[15,20]]
+
+# 10
+
+# 15 < 10 : return false
+
+
+# [0,2] old
+
+
+# ,[1,10] new 
+
+
+# if new[1] > old[0] and new[0] < old[1]: then theres overlap
+
+
+# if new[1] > old[0] and new[0] < old[1]: then theres overlap
+
+
+# # edge case what if start = end? 
