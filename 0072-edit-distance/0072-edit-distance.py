da70ca48417c17a1601962eb80e7410a1cf9d106
Sandeep Salwan <salwansandeep5@gmail.com>
2025-10-29 12:23:47 -0400

    Time: 32 ms (91.3%), Space: 19.9 MB (73.65%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,15 +1,59 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
 
-        # dp = []
         @cache
-        def recursive(i,j):
+        def dp(i, j):
+            if i >= len(word1): return len(word2)-j
+            if j >= len(word2): return len(word1)-i
+            if word1[i]== word2[j]: return dp(i+1, j+1)
+            return min(1+ dp(i+1,j+1), 1 + dp(i+1,j), 1+dp(i,j+1))
+                
+        return dp(0,0)
 
-            if i >= len(word1): return len(word2) - j
-            if j >= len(word2): return len(word1) - i
-            if word1[i] == word2[j]:
-                return recursive(i+1,j+1)
+# intention 
+# i
+# # execution
+# # j
 
-            return 1 + min(recursive(i,j+1),recursive(i+1,j), recursive(i+1,j+1))
+#  rorrr 
 
-        return recursive(0,0)
+# #  rorrrr
+# horse 
+# ros
+
+
+# #      j
+# # rorse
+# # skipreplace delete insert, 
+# # if i > len(word1): return 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # #         @cache
+# # #         def recursive(i,j):
+# # #             if i>= len(word1): return len(word2) - j
+# # #             if j>= len(word2): return len(word1) - i
+# # #             if word1[i] == word2[j]: return recursive(i+1,j+1)
+# # #             return 1+ min(recursive(i,j+1), recursive(i+1,j), recursive(i+1,j+1))
+
+# # #         return recursive(0,0)
+# # # #cat 
+
+# # # #cut
+
+
+# # # # a u
+# # # #
+
+# # # # 0 
+# # # # 
