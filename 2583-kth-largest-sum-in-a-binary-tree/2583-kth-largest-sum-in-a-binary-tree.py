1667663aaefa438fd07a7df30d01139ebbbcdb6a
Sandeep Salwan <salwansandeep5@gmail.com>
2025-11-04 18:39:54 -0500

    Time: 183 ms (34.5%), Space: 54.9 MB (76.95%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.py
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.py
@@ -7,9 +7,12 @@
 class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
         res = []
+        self.level =0
         def bfs(cur):
             seen = set()
             q = deque([cur])
+
+            
             while q:
                 cur =0 
                 for i in range(len(q)):
@@ -18,13 +21,31 @@ class Solution:
                     if child.right: q.append(child.right)
                     cur+=child.val
                 # print(cur,res)
-                res.append(cur)
+                # res.append(cur)
+                heapq.heappush(res,cur)
+                if len(res)>k:
+                    heapq.heappop(res)
+                self.level +=1
                 # print(cur,res)
         bfs(root)
+        return res[0] if k<= self.level else -1
+
+#n + hlog(h)
+
+#maxHeap
+#n + klog(h)
 
 
-        res.sort()
-        # print(res)
-        return res[-k] if k<= len(res) else -1
+# #minHeap
+# #n + hlog(k)
+
+
+
+# k=2 reaches k then pop
+# return heap[0]
+
+#         res.sort()
+#         # print(res)
+#         return res[-k] if k<= len(res) else -1
 
         
