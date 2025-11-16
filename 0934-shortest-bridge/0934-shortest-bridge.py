d948840998dd072bebbc2973ea3af66d414bf184
Sandeep Salwan <salwansandeep5@gmail.com>
2025-11-16 17:18:22 -0500

    Time: 139 ms (13.71%), Space: 19.8 MB (35.93%) - LeetHub

---
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -0,0 +1,65 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        ROW, COL = len(grid),len(grid[0])
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        seen = set()
+
+        def bfs(r,c):
+            q = deque([[r,c]])
+            grid[r][c]=0
+            res = []
+            while q:
+                for i in range(len(q)):
+                    ro,co = q.popleft()
+                    res.append([ro,co])
+                    for dr,dc in directions:
+                        ra,ca = ro+dr, co+dc
+                        if( ra in range(0,ROW) 
+                            and ca in range(0,COL) 
+                            and grid[ra][ca] == 1):
+                            q.append([ra,ca])
+                            grid[ra][ca]=0
+                            seen.add((ra,ca))
+            return res
+        self.cur =[]
+        def bfs1():
+            q = deque(self.cur)
+            # res = []
+            # print(self.cur,"a")
+            level =0
+            while q:
+                for i in range(len(q)):
+                    ro,co = q.popleft()
+                    if grid[ro][co]==1: return level -1
+                    for dr,dc in directions:
+                        ra,ca = ro+dr, co+dc
+                        if( ra in range(0,ROW) 
+                            and ca in range(0,COL) 
+                            and (ra,ca) not in seen):
+                            q.append([ra,ca])
+                            seen.add((ra,ca))
+                level+=1
+            return res
+        self.cur =0  
+
+        # lists =[]
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c]==1:
+                    self.cur = bfs(r,c)
+                    return bfs1()
+                else:
+                    seen.add((r,c))
+
+        # print(lists)
+        # minDist = float('inf')
+        # for a,b in lists[0]:
+        #     for c,d in lists[1]:
+        #         minDist = min(minDist, (abs(c-a) + abs(d-b)))
+        # return minDist-1
+        # 0,0 1,0
+
+        # 0,1. 
+
+        #     3,2
+        # 3,3 2,3
