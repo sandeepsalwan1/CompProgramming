d2cfab207f3feca384006ca97dbda2fef6a9401d
Sandeep Salwan <salwansandeep5@gmail.com>
2025-12-07 04:12:23 -0500

    Time: 0 ms (100%), Space: 18 MB (17.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,9 +1,50 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # if(len(nums)):
-        def robberMain(nums):
-            robber1,robber2 = 0,0
-            for n in nums:
-                robber1,robber2 = robber2, max(n+ robber1, robber2)
-            return robber2
-        return max(robberMain(nums[1:]), robberMain(nums[:-1]), nums[0])
+        @cache
+        def dp(i,v):
+            if i>= len(v):return 0
+            return max(dp(i+1, v), v[i]+dp(i+2,v))
+        
+        return max(nums[0],dp(0, tuple(nums[:-1])), dp(0, tuple(nums[1:])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # @cache
+        # def dp(i,newNums):
+        #     if i>= len(newNums): return 0
+        #     return max(dp(i+1, newNums), newNums[i] + dp(i+2, newNums))
+        # storedNum1, storedNum2 = tuple(nums[1:]), tuple(nums[:-1])
+        # return max(nums[0],dp(0, storedNum1), dp(0, storedNum2))
+
+        # def helper(num):
+        #     rob1, rob2= 0,0
+        #     for n in nums:
+        #         newVal = max(rob1, rob2+n)
+        #         rob1 = rob2
+        #         rob2 = newVal
+        #     return rob2
+        # @
+        # def help(num):
+
+
+        # return max(helper(nums[1:]), helper(nums[:-1]), nums[0])
